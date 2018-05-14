075afa66c039c24f96bd3c5ad21cabc18e16ccbf
JBert9000 <jbertlik@gmail.com>
2018-05-14 18:28:32 -0400

    uploading assignment, need to find out why switch won't turn on again

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
+// docoment.addEventListener("DOMContentLoaded", function() {
 
+  var button = document.querySelector('button');
+  button.addEventListener('click', function() {
+    if (document.querySelector('button').className = 'switch on') {
+      switchOff();
+    } else {
+      switchOn();
+    }
+  });
 
+  function switchOff() {
+    document.querySelector('button').className = 'switch off'
+    document.querySelector('body').className = 'dark'
+    document.querySelector('.status').innerText = "who turned off the lights?"
+  };
+
+  function switchOn() {
+    document.querySelector('button').className = 'switch on'
+    document.querySelector('body').className = 'light'
+    document.querySelector('.status').innerText = "why is it so bright in here?"
+  }
+
+// });
